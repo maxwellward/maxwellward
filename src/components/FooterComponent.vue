80029364cb72bb22b3ae8ab98c6862e8575b6524
Max Ward <max.jp.ward@gmail.com>
2025-02-21 13:55:53 -0800

    slight alignment adjustment

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,9 +1,8 @@
 <template>
 	<div class="bg-primary text-type-secondary p-7 w-full sm:w-fit">
-		<div class="flex items-center justify-between sm:items-start sm:flex-col sm:space-y-3">
-			<!-- <p>Privacy Policy</p> -->
-			<a href="https://github.com/maxwellward" target="_blank" class="hover:underline decoration-dotted">Github</a>
-			<!-- <p>Discord</p> -->
+		<div class="flex items-center justify-between sm:items-start sm:flex-col sm:space-y-3 justify-center">
+			<a href="https://github.com/maxwellward" target="_blank"
+				class="hover:underline decoration-dotted">Github</a>
 		</div>
 		<p class="flex justify-center mt-3 text-sm font-semibold">© {{ date }} - Maxwell Ward</p>
 	</div>
