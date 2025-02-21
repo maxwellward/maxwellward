bc81b75abf3e939387278e0a63264f31f3719e1e
Max Ward <max.jp.ward@gmail.com>
2025-02-21 13:43:01 -0800

    add more projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,6 +5,11 @@ export const openSourceContributions = [
 		link: 'https://github.com/peterhanania/Discord-Package',
 	},
 	{
+		title: "Streamyfin",
+		description: "Open Source Jellyfin client for Android and iOS.",
+		link: "https://github.com/streamyfin/streamyfin"
+	},
+	{
 		title: 'Slothpixel API',
 		description: 'A wrapper to simplify and clean up data returned by the official Hypixel Games API.',
 		link: 'https://github.com/slothpixel/core',
@@ -12,6 +17,16 @@ export const openSourceContributions = [
 ];
 
 export const myProjects = [
+	{
+		title: 'Dogspotter',
+		description: 'A mobile game based around taking photos of dogs in real life to gain points and climb the leaderboard.',
+		link: 'https://github.com/maxwellward/dogspotter-mobile',
+	},
+	{
+		title: 'Pokémon GO Data Viewer',
+		description: 'Data visualiser for Pokémon GO data packages.',
+		link: 'https://github.com/maxwellward/pogo-dump-viewer',
+	},
 	{
 		title: 'Asteroids',
 		description: 'A clone of the 1979 hit game "Asteroids" by Lyle Rains and Ed Logg (Atari). Built in Unity with C#.',
