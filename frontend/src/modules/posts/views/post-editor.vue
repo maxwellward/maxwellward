ae731590bc0b66529788c074f8634fe3a2d889dd
Max Ward <max.jp.ward@gmail.com>
2025-04-02 22:36:39 -0500

    add date picker to post editor

--- frontend/src/modules/posts/views/post-editor.vue
+++ frontend/src/modules/posts/views/post-editor.vue
@@ -5,6 +5,8 @@
 			class="w-4/5 border border-type-secondary py-1 px-2 rounded-md text-white" />
 		<input v-model="post.description" type="text" placeholder="Description"
 			class="w-4/5 border border-type-secondary py-1 px-2 rounded-md text-white" />
+		<input v-model="selectedDate" type="date" placeholder="Date"
+			class="w-4/5 border border-type-secondary py-1 px-2 rounded-md text-white" @change="updatePostDate" />
 		<m-markdown-editor v-model="post.content" class="w-4/5 text-white" />
 		<button class="border-1 border-white rounded-md mt-2 px-4 py-2 hover:cursor-pointer hover:font-bold"
 			@click="save">
@@ -41,11 +43,21 @@ onMounted(async () => {
 
 		if (p) {
 			post.value = p;
+			selectedDate.value = new Date(p.date.seconds * 1000).toISOString().split('T')[0];
 		} else {
 			throw new Error('Post not found');
 		}
 	}
 });
+
+const selectedDate = ref();
+
+const updatePostDate = () => {
+	if (selectedDate.value) {
+		const date = new Date(selectedDate.value + "T00:00:00");
+		post.value.date = new Timestamp(Math.floor(date.getTime() / 1000), 0);
+	}
+}
 
 const save = async () => {
 	await postStore.savePost(post.value);
